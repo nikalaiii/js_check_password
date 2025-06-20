3c2aaf43e6e8d2204d91f205a27d08ead3d2fc03
Nikolay <mikalaj.krutak@gmail.com>
2025-06-20 17:13:48 +0200

    ready tests

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,40 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = checkPassword('123');
 
+    expect(typeof result).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
+    const result = checkPassword('Password1!');
 
+    expect(result).toBe(true);
   });
 
-  // write more tests here
+  it('should accept max 16 charakters in password', () => {
+    const result = checkPassword(
+      'Password1!Password1!Password1!Password1!Password1!'
+    );
+
+    expect(result).toBe(false);
+  });
+
+  it('should require min 1 upper-case letter', () => {
+    const result = checkPassword('password1!');
+
+    expect(result).toBe(false);
+  });
+
+  it('should require min one special charakter', () => {
+    const result = checkPassword('Password11');
+
+    expect(result).toBe(false);
+  });
+
+  it('should require min one digit', () => {
+    const result = checkPassword('Password!!');
+
+    expect(result).toBe(false);
+  });
 });
